7eefdeae9aed888737825aa74f774c186be64c1c
MrMiff <49294853+MrMiff@users.noreply.github.com>
2025-04-07 12:42:12 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  fromPrivateKey: "PRIVATE_KEY_DOMPET_KENA_DRAIN",
+  toAddress: "WALLET_SAFE",
+  tokenContract: "TRC20_CONTRACT_ADDRESS",
+  tronNode: "https://api.trongrid.io"
+};
